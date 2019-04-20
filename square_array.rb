09fbfc4a4d032c1830521d408f68ed098ac00e20
Dennis <dennis.basyrov@gmail.com>
2019-04-20 15:21:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,9 @@ def square_array(array)
   array.each { |i| new_array << i * i }
   return new_array
 end
+
+def square_array(array)
+  new_array = []
+  array.each { |i| new_array << i * i }
+  return new_array
+end
